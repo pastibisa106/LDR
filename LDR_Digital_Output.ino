d578da01ce354fa8db2e1e1529277f96548e1cd1
pastibisa106 <35872007+pastibisa106@users.noreply.github.com>
2025-05-08 13:14:44 +0700

    Add files via upload

---
+++ LDR_Digital_Output.ino
@@ -0,0 +1,22 @@
+int cahaya = 5;  //inisial pin sensor cahaya
+int led =13;      //inisial pin led
+
+void setup() {
+    // put your setup code here, to run once:
+    pinMode(cahaya,INPUT);  //pin sensor cahaya sebagai input
+    pinMode (led,OUTPUT);   //inisial led sebagai output
+    Serial.begin(9600);     //serial komunikasi atau baudrate
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+    int data = digitalRead(cahaya); //inisial bahwa data sama dengan membaca sensor cahaya
+    Serial.println(data);  //menampilkan nilai sensor di serial monitor
+  
+    if (data == 0){      //jika sensor mendeteksi tidak ada cahaya
+      digitalWrite(led,HIGH); //led on
+    }
+    else{       //jika ada cahaya
+      digitalWrite(led,LOW);    //led off
+    }
+}
